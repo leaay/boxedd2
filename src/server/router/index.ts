77514d77601218da3903e16231d2abc2a291b886
leaay <wiktorgs98@gmail.com>
2022-09-10 22:10:37 +0200

    typeerror

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,14 +2,13 @@
 import { createRouter } from "./context";
 import superjson from "superjson";
 import { testRouter } from "./test";
-
 import { protectedExampleRouter } from "./protected-example-router";
 
 export const appRouter = createRouter()
   .transformer(superjson)
   .merge("auth.", protectedExampleRouter)
   .merge("test.", testRouter);
-;
+
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
